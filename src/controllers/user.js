ee0697867bee75ade15eb5195d260a668e4ab8b4
Daniel Zermeño <daniel.zermeno@softwaria.mx>
2020-11-29 17:55:37 -0600

    User: Creation of hashing password and validation for crreation of users

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,15 +1,25 @@
 import mongoose from 'mongoose';
 import User from '../models/user';
+import bcrypt from 'bcrypt'; //Bcrypt for the hash of the password
+
+//SALT ROUNDS for the random hash
+const BCRYPT_SALT_ROUNDS = 12;
 
 export const registerUser = async (req, res) => {
   const user = req.body;
   try{
-    const newUser = new User(user);
+    const userExist = await User.findOne({email: user.email});
+    if(userExist)
+      return res.status(400).json({message: 'User exist'});
+
+    //hashing the password
+    const hashPassword = await bcrypt.hash(user.password, BCRYPT_SALT_ROUNDS);
+    const newUser = new User({...user, password: hashPassword});
 
     const result = await newUser.save();
 
-    res.status(200).json(newUser);
+    res.json(newUser);
   }catch(error){
-    res.status(500).json(error.message);
+    res.json(error.message);
   }
 }
